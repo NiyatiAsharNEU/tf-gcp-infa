fadbe3dfb56d9af69eeb0e4b10f63d1ee7fbf31c
Niyati Ashar <niyatiashar17@gmail.com>
2024-02-22 17:01:38 -0500

    terraform code

--- main.tf
+++ main.tf
@@ -33,3 +33,60 @@ resource "google_compute_route" "webapp_route" {
   priority         = 1000
   tags             = ["webapp"]
 }
+
+resource "google_compute_firewall" "webapp_firewall" {
+  name    = var.firewall_name
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = [var.app_port]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["webapp"]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "google_compute_firewall" "ssh_firewall" {
+  name    = var.ssh_firewall_name
+  network = google_compute_network.vpc.name
+
+  deny {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["webapp"]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "google_compute_instance" "default" {
+  name         = var.vm_name
+  machine_type = var.vm_machine_type
+  zone         = var.vm_zone
+  tags         = var.vm_tags
+
+  boot_disk {
+    initialize_params {
+      image = var.vm_image
+      size  = var.vm_size
+      type  = var.vm_type
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc.self_link
+    subnetwork = google_compute_subnetwork.webapp_subnet.self_link
+    access_config {
+    }
+
+  }
+}
--- variables.tf
+++ variables.tf
@@ -28,3 +28,37 @@ variable "db_subnet" {
 variable "webapp_route" {
 }
 
+variable "app_port"{
+}
+
+variable "firewall_name"{
+
+}
+
+variable "ssh_firewall_name" {
+
+}
+
+variable "vm_name"{
+
+}
+variable "vm_size"{
+
+}
+
+variable "vm_type"{
+
+}
+variable "vm_machine_type" {
+  
+}
+
+variable "vm_image" {
+  
+}
+variable "vm_zone" {
+  
+}
+variable "vm_tags" {
+  
+}
